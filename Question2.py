4f55299a063e2f034b9ca5bc0860eb4b38f725b3
sai14199 <105456780+sai14199@users.noreply.github.com>
2022-08-02 03:08:52 +0530

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -1,5 +1,6 @@
 #What is the difference between string and variable?
 """
+Ans:
 String: Type of a variable which can hold a character or a array of characters.
 Variable: A variable is a named memory address that can hold a value.
 """
