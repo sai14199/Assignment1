c7c45172f29ded85a79d701ffe9f1c36193e23dd
sai14199 <105456780+sai14199@users.noreply.github.com>
2022-08-02 03:09:36 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,5 +1,6 @@
 #What is an expression made up of? What do all expressions do?
 """
+Ans:
 What is an expression made up of variables or constants, and operators.
 Expressions are evaluated to find an output which can be used as values to other variables.
 
